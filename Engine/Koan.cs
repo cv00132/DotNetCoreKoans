f1bf3c3bfe3364c5e3d8506da5b4886c268c6359
mdclement <mike@softwareontheside.com>
2017-06-01 15:57:53 -0400

    Rearranged order for better leveling for new programmers.

--- Engine/Koan.cs
+++ Engine/Koan.cs
@@ -2,14 +2,15 @@ using System;
 
 namespace DotNetCoreKoans.Engine
 {
-    public class Koan
-    {
-        public static dynamic FILL_ME_IN = new Object();
-        public virtual void Setup() { }
-        public virtual void TearDown() { }
+  public class Koan
+  {
+    public static dynamic FILL_ME_IN = new Object();
+    public static string FILL__ME_IN = "Fill me in";
+    public virtual void Setup() { }
+    public virtual void TearDown() { }
 
-    }
+  }
 
-    //This is just used when we need a type
-    public abstract class FillMeIn { }
+  //This is just used when we need a type
+  public abstract class FillMeIn { }
 }
--- Koans/PathToEnlightenment.cs
+++ Koans/PathToEnlightenment.cs
@@ -3,28 +3,28 @@ using DotNetCoreKoans.Engine;
 
 namespace DotNetCoreKoans.Koans
 {
-    public class PathToEnlightenment : Path
+  public class PathToEnlightenment : Path
+  {
+    public PathToEnlightenment()
     {
-        public PathToEnlightenment()
-        {
-            Types = new Type[] {
-				typeof(AboutAsserts),
-				typeof(AboutNull),
-				typeof(AboutArrays),
-				typeof(AboutArrayAssignment),
-				typeof(AboutStrings),
-                typeof(AboutFloats),
-				typeof(AboutInheritance),
-                typeof(AboutMethods),
-                typeof(AboutControlStatements),
-                typeof(AboutGenericContainers),
-		typeof(AboutBitwiseAndShiftOperator)
-                //TODO: disabled due to missing functionality in netcoreapp1.0
-                // it will be available in 1.1 see:
-                // https://github.com/dotnet/coreclr/blob/master/src/mscorlib/src/System/Array.cs#L1005
-                //typeof(AboutDelegates),
-                //typeof(AboutLambdas)
-                };
-        } 
+      Types = new Type[] {
+        typeof(AboutAsserts),
+        typeof(AboutStrings),
+        typeof(AboutFloats),
+        typeof(AboutArrays),
+        typeof(AboutArrayAssignment),
+        typeof(AboutGenericContainers),
+        typeof(AboutControlStatements),
+        typeof(AboutMethods),
+        typeof(AboutInheritance),
+        typeof(AboutNull),
+        typeof(AboutBitwiseAndShiftOperator)
+        //TODO: disabled due to missing functionality in netcoreapp1.0
+        // it will be available in 1.1 see:
+        // https://github.com/dotnet/coreclr/blob/master/src/mscorlib/src/System/Array.cs#L1005
+        //typeof(AboutDelegates),
+        //typeof(AboutLambdas)
+        };
     }
+  }
 }
